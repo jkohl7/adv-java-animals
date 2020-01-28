564dae6664d7abd9f49fe8c36065170a6993b47a
jkohl7 <jkohl7@my.wctc.edu>
2020-01-28 15:06:55 -0600

    updated userChoice

--- src/us/mattgreen/userChoice.java
+++ src/us/mattgreen/userChoice.java
@@ -9,17 +9,33 @@ public class userChoice {
         System.out.println("What type of animal do you want to create?");
         System.out.println("1: Dog");
         System.out.println("2: Cat");
-        System.out.println("3: Person");
-        int num = Integer.parseInt(keyboard.nextLine());
-//        switch(num){
-//            case 1:
-//                break;
-//            case 2:
-//                break;
-//            case3:
-//                break;
-//
-//        }
+        System.out.println("3: Teacher");
+        int num = keyboard.nextInt();
+        switch(num){
+            case 1:
+                Scanner keyboard1 = new Scanner(System.in);
+                System.out.println("What is the dog's name?");
+                String dogName = keyboard1.next();
+                System.out.println("Is the dog friendly? (Y/N)");
+                boolean friendly = true; //still need to work on this
+                Dog dog = new Dog (friendly, dogName);
+            case 2:
+                Scanner keyboard2 = new Scanner(System.in);
+                System.out.println("What is the cat's name?");
+                String catName = keyboard2.next();
+                System.out.println("How many mice has your cat killed?");
+                int killed = keyboard2.nextInt();
+                Cat cat = new Cat(killed, catName);
+                break;
+            case 3:
+                Scanner keyboard3 = new Scanner(System.in);
+                System.out.println("What is the teacher's name?");
+                String teacherName = keyboard3.next();
+                System.out.println("What is the age of the teacher?");
+                int age = keyboard3.nextInt();
+                Teacher teacher = new Teacher(age, teacherName);
+                break;
+        }
 
 
     }
